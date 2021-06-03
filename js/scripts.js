e8f7130aa4496baa69b7bef7df1151362ae51753
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-06-03 08:48:17 -0700

    Link CSS to index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,20 @@
 function cap() {
   const lastLetter = firstSentence.charAt(firstSentence.length-1).toUpperCase();
   const firstLetter = firstSentence.charAt(0).toUpperCase();
-  // console.log(firstLetter + lastLetter);
   return firstLetter + lastLetter;
 }
 function reverse() {
   let reverse1 = reverseLetters.charAt(reverseLetters.length-1);
   let reverse2 = reverseLetters.charAt(0);
-  // console.log(reverse1 + reverse2);
   return reverse1 + reverse2;
 }
 function addTogether() {
-  // console.log(firstSentence + reverseLetterString);
   return firstSentence + reverseLetterString;
 }
 function letterEquation() {
   const countSentence = parseInt(firstSentence.length);
   const divide = countSentence / 2;
   const newLetter = firstSentence.charAt(divide);
-  // console.log(newLetter + newReverseLetterString);
   return newLetter + newReverseLetterString;
 }
 function reverseAll() {
@@ -27,29 +23,20 @@ function reverseAll() {
   for (let i = lastOne.length-1; i>=0; i--) {
     finalReverse += lastOne[i]
   }
-  // console.log(finalReverse);
   return(finalReverse);
 }
 
 let firstSentence = prompt("Enter a sentence:");
-  const reverseLetters = cap();
-  const reverseLetterString = reverse();
-  const newReverseLetterString = addTogether();
-  const lastOne = letterEquation();
-  const ultimateOne = reverseAll();
+const reverseLetters = cap();
+const reverseLetterString = reverse();
+const newReverseLetterString = addTogether();
+const lastOne = letterEquation();
+const ultimateOne = reverseAll();
 
 $(document).ready(function() {
 
-  // let firstSentence = prompt("Enter a sentence:");
-  // console.log(firstSentence);
-  // const reverseLetters = cap();
-  // const reverseLetterString = reverse();
-  // const newReverseLetterString = addTogether();
-  // const lastOne = letterEquation();
-  // const ultimateOne = reverseAll();
-
-  $("#sentence1").text(firstSentence).hide();
-  $("#sentence2").text(ultimateOne).hide();
+  $("#sentence1").text(firstSentence);
+  $("#sentence2").text(ultimateOne);
 
   $("#originalWord").click(function() {
     $("#sentence2").hide();
