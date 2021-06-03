1a737fe5c7b152c9bc7c46e732a0fdb00a3ae0e5
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-06-03 06:37:58 -0700

    Add two functions to reverse letters and add original sentence to it.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,22 @@ function cap() {
   const lastLetter = firstSentence.charAt(firstSentence.length-1).toUpperCase();
   const firstLetter = firstSentence.charAt(0).toUpperCase();
   console.log(firstLetter + lastLetter);
+  return firstLetter + lastLetter;
+}
+function reverse() {
+  let reverse1 = reverseLetters.charAt(reverseLetters.length-1);
+  let reverse2 = reverseLetters.charAt(0);
+  console.log(reverse1 + reverse2);
+  return reverse1 + reverse2;
+}
+function addTogether() {
+  console.log(firstSentence + reverseLetterString);
+  return firstSentence + reverseLetterString;
 }
 
-const firstSentence = prompt("Enter a sentence:");
+let firstSentence = prompt("Enter a sentence:");
 console.log(firstSentence);
-cap(firstSentence);
+const reverseLetters = cap();
+const reverseLetterString = reverse();
+addTogether();
 
